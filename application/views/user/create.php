e01f27f143e943c55025568e813609755cd8ed15
albunicoleta <albunicoleta@gmail.com>
2013-02-10 21:19:53 +0200

    fixed small bugs in views

--- application/views/user/create.php
+++ application/views/user/create.php
@@ -1,12 +1,12 @@
 
-<form class="form-center" action="<?php echo base_url('users/postCreate'); ?>" method="post" />
-<label for="username">Username</label>
-<input type="text" name="username"/>
-<label for="password">Password</label>
-<input type="password" name="password"/>
-<label for="confirm password">Confirm password</label>
-<input type="password" name="confirm_password"/>
-<label for="email"> E-mail </label>
-<input type="text" name="email_adress"/>
-<input type="submit" name="Register" value="Register"/>           
+<form class="form-center" action="<?php echo base_url('users/postCreate'); ?>" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username"/>
+    <label for="password">Password</label>
+    <input type="password" name="password"/>
+    <label for="confirm password">Confirm password</label>
+    <input type="password" name="confirm_password"/>
+    <label for="email"> E-mail </label>
+    <input type="text" name="email_adress"/>
+    <input type="submit" name="Register" value="Register"/>           
 </form>  
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,12 +1,7 @@
-<html>
-    <body>
-        <form class="form-center" action="<?php echo base_url('users/postLogin'); ?>" method="post" />
-        <label for="username">Username</label>
-        <input type="text" name="username"/>
-        <label for="password">Password</label>
-        <input type="password" name="password"/>   
-        <input type="submit" name="Register" value="Log In"/>
-    </form>  
-    </body>    
-</html>
-<?php echo $this->session->userdata('username') ?>
+<form class="form-center" action="<?php echo base_url('users/postLogin'); ?>" method="post" >
+    <label for="username">Username</label>
+    <input type="text" name="username"/>
+    <label for="password">Password</label>
+    <input type="password" name="password"/>   
+    <input type="submit" name="Register" value="Log In"/>
+</form>  
