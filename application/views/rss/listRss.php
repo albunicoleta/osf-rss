4278fc38174553146be446dcacf69b7e262598f2
albunicoleta <albunicoleta@gmail.com>
2013-02-21 21:17:06 +0200

    #13

---
+++ application/views/rss/listRss.php
@@ -0,0 +1,5 @@
+<?php
+foreach ($data as $row){
+    echo $row . '<br/>';
+}
+?>
--- application/views/rss/rssSources.php
+++ application/views/rss/rssSources.php
@@ -1,4 +1,5 @@
 <h2> Rss Sources! </h2> <br/><br/>
- <a href="<?php echo base_url('rssFeed /changeRss')?>">Change Rss sources</a><br/>
- <a href="<?php echo base_url('rssFeed/addRss')?>">Add Rss sources</a><br/> 
- <a href="<?php echo base_url('rssFeed/deleteRss')?>">Delete Rss sources</a><br/>
+<a href="<?php echo base_url('rssFeed/listRss') ?>">List All Rss sources</a><br/>
+<a href="<?php echo base_url('rssFeed/changeRss') ?>">Change Rss sources</a><br/>
+<a href="<?php echo base_url('rssFeed/addRss') ?>">Add Rss sources</a><br/> 
+<a href="<?php echo base_url('rssFeed/deleteRss') ?>">Delete Rss sources</a><br/>
