7957a4ad320096c975338f1c2214e4d90e1de767
albunicoleta <albunicoleta@gmail.com>
2013-02-21 21:15:53 +0200

    #15

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,7 +1,11 @@
 <head>
     <title> Header </title>
+    
     <script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/jquery-1.9.1.min.js" ></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/validatr.js" ></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/bootstrap.js" ></script>
+    
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/validatr.css" /> 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/style.css" /> 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.css" /> 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap-responsive.css" />
--- application/views/user/create.php
+++ application/views/user/create.php
@@ -1,12 +1,18 @@
-
-<form class="form-center" action="<?php echo base_url('users/postCreate'); ?>" method="post">
+<script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/jquery-1.9.1.min.js" ></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/jquery.validate.js"></script>
+		<script type="text/javascript">
+			 $(document).ready(function(){
+				$("#form-create").validate();
+			 });
+</script>
+<form id="form-create" class="form-center" action="<?php echo base_url('users/postCreate'); ?>" method="post">
     <label for="username">Username</label>
-    <input type="text" name="username"/>
+    <input type="text" name="username" class="required" minlength="2"/>
     <label for="password">Password</label>
-    <input type="password" name="password"/>
+    <input type="password" name="password" class="required"/>
     <label for="confirm password">Confirm password</label>
-    <input type="password" name="confirm_password"/>
+    <input type="password" name="confirm_password" class="required"/>
     <label for="email"> E-mail </label>
-    <input type="text" name="email_adress"/>
-    <input type="submit" name="Register" value="Register"/>           
-</form>  
+    <input type="text" name="email_adress" class="required" />
+    <input type="submit" name="Register" value="Register" />           
+</form>
