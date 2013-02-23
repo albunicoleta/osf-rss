0fa9be91739ac0834d12e7870bb5964cc2ca326e
albunicoleta <albunicoleta@gmail.com>
2013-02-23 14:00:45 +0200

    #17 added migation file 5; fixed bug in file migration 4

--- application/migrations/005_create_unique_key.php
+++ application/migrations/005_create_unique_key.php
@@ -5,15 +5,15 @@ class Migration_Create_unique_key extends CI_Migration {
     public function up()
     {
 
-        $sql = "ALTER TABLE rss
-                ADD UNIQUE (link)";
+        $sql = "ALTER TABLE users
+                ADD UNIQUE `unique`(username, email)";
         $this->db->query($sql);
     }
 
     public function down()
     {
         $sql = "ALTER TABLE users
-                DROP INDEX `link`";
+                DROP INDEX `unique`";
         $this->db->query($sql);
     }
 
