b5437963f7a0cc6e0c6149e0819cbd69544732dd
albunicoleta <albunicoleta@gmail.com>
2013-02-24 16:44:13 +0200

    bug fix: removed already loaded javascript files

--- application/views/user/create.php
+++ application/views/user/create.php
@@ -1,5 +1,3 @@
-<script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/jquery-1.9.1.min.js" ></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>bootstrap/js/jquery.validate.js"></script>
 <script>
     $(document).ready(function(){
         $("#form-create").validate({
