a5b062e8fd0e1c26b133c067a187b495a48fa7ee
albunicoleta <albunicoleta@gmail.com>
2013-02-24 16:47:42 +0200

    #11 added js validation

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -1,9 +1,14 @@
-<form action="<?php echo base_url('users/postEdit'); ?>" method="post">
+<script>
+    $(document).ready(function(){
+        $("#form-edit").validate();
+    });
+</script>
+<form id="form-edit" action="<?php echo base_url('users/postEdit'); ?>" method="post">
     <label for="username"> New username </label>
     <input type="text" name="username"/>
     <label for="password>"> New password </label>
     <input type="password" name="password"/>
     <label for="email>"> New email </label>
-    <input type="text" name="email"/>
+    <input type="text" name="email" class="email"/>
     <input type="submit" name="submit" value="Submit"/>
 </form>
