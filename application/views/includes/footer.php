c3199d30982b23018849a37aa07dc7308b971e54
albunicoleta <albunicoleta@gmail.com>
2013-02-24 18:49:28 +0200

    closes #22

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,1 +1,4 @@
-<h1> This is the footer! </h1>
+<div class="container">
+    <p class="muted-credit">Developed by Albu Nicoleta 2013 (<a href="https://github.com/albunicoleta/osf-rss/">Github</a>)</p>
+</div>
+
--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -1,41 +1,46 @@
 <html>
     <?php $this->load->view('includes/header'); ?>
     <body>
-        <div class="navbar"> 
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="brand" href="<?php echo base_url('users/homepage')?>">OSF Global</a>
-<!--                    <h3 class="brand"> OSF Global </h3>-->
-                    <?php /* if username exists in session then we have a logged in user*/ ?>
-                    <?php if ($this->session->userdata('username')): ?>
-                        <div class="navbar-text pull-right">
-                            Hello <?php echo $this->session->userdata('username') ?>!
-                            <a href="<?php echo base_url('users/edit')?>">Edit account</a>
-                            <a href="<?php echo base_url('rssFeed/rssSources')?>">Rss Sources</a>
-                            <a href="<?php echo base_url('users/logout')?>">Logout</a>                          
+        <div id="wrap">
+            <div class="container">
+                <div class="navbar"> 
+                    <div class="navbar-inner">
+                        <div class="container">
+                            <a class="brand" href="<?php echo base_url('users/homepage') ?>">OSF Global</a>
+                            <!--                    <h3 class="brand"> OSF Global </h3>-->
+                            <?php /* if username exists in session then we have a logged in user */ ?>
+                            <?php if ($this->session->userdata('username')): ?>
+                                <div class="navbar-text pull-right">
+                                    Hello <?php echo $this->session->userdata('username') ?>!
+                                    <a href="<?php echo base_url('users/edit') ?>">Edit account</a>
+                                    <a href="<?php echo base_url('rssFeed/rssSources') ?>">Rss Sources</a>
+                                    <a href="<?php echo base_url('users/logout') ?>">Logout</a>                          
+                                </div>
+
+                            <?php else: ?>
+                                <form class="navbar-form pull-right" action="<?php echo base_url('users/postLogin'); ?>" method="post">
+                                    <input type="text" name="username" placeholder="Username"/>
+                                    <input type="password" name="password" placeholder="Password"/>
+                                    <input class="btn" type="submit" value="Sign In"/>
+                                    <a class="btn" href="<?php echo base_url('users/create') ?>"> Register </a>
+                                </form>
+                            <?php endif; ?>                        
                         </div>
-                        
-                    <?php else: ?>
-                        <form class="navbar-form pull-right" action="<?php echo base_url('users/postLogin'); ?>" method="post">
-                            <input type="text" name="username" placeholder="Username"/>
-                            <input type="password" name="password" placeholder="Password"/>
-                            <input class="btn" type="submit" value="Sign In"/>
-                            <a class="btn" href="<?php echo base_url('users/create')?>"> Register </a>
-                        </form>
-                    <?php endif; ?>                        
+
+                    </div>    
                 </div>
-                
-            </div>    
+                <div class="container">
+                    <?php if ($this->session->flashdata('message')): ?>
+                        <div class="alert"><?php echo $this->session->flashdata('message'); ?></div>
+                    <?php endif; ?>
+                    <?php $this->load->view($main_content); ?>
+                </div>
+            </div>
+            <div id="push"></div>
         </div>
-        <div class="container">
-            <?php if($this->session->flashdata('message')): ?>
-                <div class="alert"><?php echo $this->session->flashdata('message'); ?></div>
-            <?php endif; ?>
-            <?php $this->load->view($main_content); ?>
-        </div>
-        <div class="footer">
-        <?php $this->load->view('includes/footer'); ?>
+        <div id="footer">
+            <?php $this->load->view('includes/footer'); ?>
         </div>    
     </body>
-    
+
 </html>
