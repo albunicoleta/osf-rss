bcfb6d4adefd57c1b479d7c241d1d29de75c8d73
albunicoleta <albunicoleta@gmail.com>
2013-02-27 22:00:07 +0200

    #13 added edit button for rss links

--- application/views/rss/listRss.php
+++ application/views/rss/listRss.php
@@ -1,6 +1,34 @@
+<script type="text/javascript">
+    function pencilClicked() {
+        var html = $(this).next('span').html();
+        var editableText = $("<textarea />");
+        editableText.val(html);
+        $(this).next('span').replaceWith(editableText);
+        editableText.focus();
+        // setup the blur event for this new textarea
+        editableText.blur(editableTextBlurred);
+    }
+    
+    function editableTextBlurred() {
+        var html = $(this).val();
+        var viewableText = $("<span>");
+        viewableText.html(html);
+        $(this).replaceWith(viewableText);
+        // setup the click event for this new div
+        viewableText.click(pencilClicked);
+    }
+
+    $(document).ready(function() {
+        $(".icon-pencil").click(pencilClicked);
+    });
+    
+</script>
 <ul class="nav">
     <?php foreach ($data as $row): ?>
-    <li><?php echo $row;?></li>
+        <li>
+            <i class="icon-pencil"></i>
+            <span><?php echo $row; ?></span>
+        </li>
     <?php endforeach; ?>
 </ul>
 <div class="pagination">
