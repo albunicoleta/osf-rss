2bd10bae727b0dc6b26a71cc6ad39744a5d3ca0d
albunicoleta <albunicoleta@gmail.com>
2013-02-28 22:22:26 +0200

    fixed mysql bug

--- application/migrations/003_create_tables2.php
+++ application/migrations/003_create_tables2.php
@@ -10,7 +10,7 @@ class Migration_Create_tables2 extends CI_Migration {
         CREATE TABLE IF NOT EXISTS users_rss(
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         user_id INT NOT NULL,
-        rss_id INT NOT NULL,
+        rss_id INT NOT NULL
         )";
         $this->db->query($sql);
     }
