da98b908980b6ab8723ae7b6af7b7466c40dcc9a
albunicoleta <albunicoleta@gmail.com>
2013-03-05 15:40:42 +0200

    #24 created admin view

---
+++ application/views/admin/login.php
@@ -0,0 +1,7 @@
+<form action="<?php echo base_url('admin/postLogin'); ?>" method="post" >
+    <label for="username">Admin</label>
+    <input type="text" name="username"/>
+    <label for="password">Password</label>
+    <input type="password" name="password"/>   
+    <input type="submit" name="login" value="Log In"/>
+</form>  
