fc8c803b2c4b56eff9e885827271b855812e09f0
albunicoleta <albunicoleta@gmail.com>
2013-03-06 20:37:52 +0200

    closes #31

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -18,6 +18,18 @@ class Admin extends OSF_Controller {
     
     public function postLogin()
     {
+        //1. get post data
+        $postData = $this->input->post();
+        $this->load->model('administrator');
+        
+        if ($this->administrator->canLogin($postData["username"], $postData["password"])) {
+            /* we set user data on session */
+            $this->session->set_flashdata('message', 'Welcome admin!');
+        } else {
+            $this->session->set_flashdata('message', 'Your username or password is incorrect!');
+        }
+        redirect(base_url());
+        
         
     }    
 }
--- application/models/administrator.php
+++ application/models/administrator.php
@@ -8,6 +8,16 @@
  */
 class Administrator extends CI_Model{
     
-    
+    public function canLogin($username, $password)
+    {
+        $this->load->database();
+        $query = $this->db->get_where('administrators', array('username'=> $username, 'password'=>$password));
+        /* if it founds at least one row then its valid */
+        if ($query->num_rows()) {
+            return TRUE;
+        }
+
+        return FALSE;
+    }
 }
 
