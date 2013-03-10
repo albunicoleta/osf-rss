e9eea9df775f869ea448e047a8e5342f30cd694b
albunicoleta <albunicoleta@gmail.com>
2013-03-10 15:48:35 +0200

    added 'Logged in as xxx' in navbar.php

--- application/views/admin_includes/navbar.php
+++ application/views/admin_includes/navbar.php
@@ -1,4 +1,5 @@
-<?php if ($this->session->userdata(Admin::SESSION_KEY)): ?>
+<?php if ($adminUsername = $this->session->userdata(Admin::SESSION_KEY)): ?>
+    <div style="float:left;margin-top:10px;">Logged in as <span style="color:blue;"><?php echo $adminUsername; ?></span></div>
     <div class="navbar-text pull-right"> 
         <a class="btn" href="<?php echo base_url('admin/register') ?>">Register new admin</a> 
         <a class="btn" href="<?php echo base_url('admin/logout') ?>">Logout</a> 
