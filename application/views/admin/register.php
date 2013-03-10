ad85248580cd620b9b8ad61dabc85cde1516d653
albunicoleta <albunicoleta@gmail.com>
2013-03-10 21:07:03 +0200

    add js validation for admin/registration

--- application/views/admin/register.php
+++ application/views/admin/register.php
@@ -1,5 +1,17 @@
+<script>
+    $(document).ready(function(){
+        $("#admin-register").validate({
+            rules: {
+                password: "required",
+                confirm_password: {
+                    equalTo: "#password"
+                }
+            }
+        });
+    });
+</script>
 <div class="form-horizontal"> 
-    <form action="<?php echo base_url('admin/postRegister'); ?>" method="post" >
+    <form id="admin-register" action="<?php echo base_url('admin/postRegister'); ?>" method="post" >
         <label for="username">Username</label>
         <input type="text" name="username" class="required" minlength="2"/>
         <label for="password">Password</label>
